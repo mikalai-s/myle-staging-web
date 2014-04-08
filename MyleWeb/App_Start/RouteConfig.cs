0561f97a53f8158a0707ee8fda6a28d0752ef10a
Mikalai Silivonik <mikalais@pandell.com>
2014-04-08 00:31:19 -0600

    inital commit

---
+++ MyleWeb/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MyleWeb
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            /*
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            */
+            routes.MapHttpRoute(
+                name: "DefaultApi2",
+                routeTemplate: "{controller}/{filename}",
+                defaults: new { filename = RouteParameter.Optional }
+
+            );
+        }
+    }
+}
---
+++ MyleWeb/Controllers/FilesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MyleWeb.Controllers
+{
+    public class FilesController : ApiController
+    {
+        private static TraceSource TraceSource = new TraceSource("FilesController");
+
+        //
+        // Post: /Files/
+        public HttpResponseMessage Post([FromUri]string filename)
+        {
+            var task = this.Request.Content.ReadAsStreamAsync();
+            task.Wait();
+            var requestStream = task.Result;
+
+            try
+            {
+                var dir = Path.Combine(HttpContext.Current.Server.MapPath("~/Uploads/"), DateTime.UtcNow.ToString("yyyy-MM-dd"));
+                Directory.CreateDirectory(dir);
+                var fileStream = File.Create(Path.Combine(dir, filename));
+
+                requestStream.CopyTo(fileStream);
+                fileStream.Close();
+                requestStream.Close();
+            }
+            catch (Exception e)
+            {
+                TraceSource.TraceData(TraceEventType.Error, 0, e.ToString());
+                throw e;//new HttpResponseException(HttpStatusCode.InternalServerError);
+            }
+
+            var response = new HttpResponseMessage();
+            response.StatusCode = HttpStatusCode.Created;
+            return response;
+        }
+	}
+}
